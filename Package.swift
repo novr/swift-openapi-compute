5755e63cc4e4fba8c82fd67a42bfa42b8411a28c
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2023-09-08 16:23:38 +0900

    bump: version up

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["OpenAPICompute"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/swift-cloud/Compute", from: "2.14.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/swift-cloud/Compute", from: "2.17.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
